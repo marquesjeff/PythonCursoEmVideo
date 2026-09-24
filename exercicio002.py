817c4199b7f78c7a311efcab36bc83e95ed63ba7
Jefferson Marques <166242974+marquesjeff@users.noreply.github.com>
2026-09-24 17:44:00 -0300

    ex 1-4

---
+++ exercicio002.py
@@ -0,0 +1,2 @@
+nome = str(input("Digite seu nome: ")).strip().capitalize()
+print(f"Prazer em recebê-lo(a), {nome}!")
---
+++ exercicio003.py
@@ -0,0 +1,6 @@
+#somando dois números
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+soma = num1 + num2
+print(f"O resultado da soma entre {num1} e {num2} é {soma}.")
